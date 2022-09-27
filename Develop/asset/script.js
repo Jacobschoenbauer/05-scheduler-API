d86a81b617889d86e5d17b1e80a122010a8dab82
Jacobschoenbauer <jhwt65@gmail.com>
2022-09-27 10:51:41 -0500

    added comments

---
+++ Develop/asset/script.js
@@ -0,0 +1,10 @@
+//i open planner
+// the current day is displayed at the top of calendar
+// when scroll doen presented with blocks for standard buisness hours
+// i view the time blocks for standard buisness hours
+//each time block is color coded to indicate whether it is in the past present future
+//click block can enter event
+ // click button save button for the block the textfor the event is savedin local storage
+ // i refresh page and events stay on page
+ var today = moment();
+ $("#currentDay").text(today.format("MMM Do, YYYY"));
