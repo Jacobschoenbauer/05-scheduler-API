4549d2bd45b22bf6c273f6662b2e63b5a8c87457
Jacobschoenbauer <jhwt65@gmail.com>
2022-09-29 12:17:32 -0500

    chaged html and used map method to repest code using jquery

--- Develop/asset/script.js
+++ Develop/asset/script.js
@@ -1,10 +1,37 @@
 //i open planner
 // the current day is displayed at the top of calendar
-// when scroll doen presented with blocks for standard buisness hours
+// when scroll down presented with blocks for standard buisness hours
 // i view the time blocks for standard buisness hours
 //each time block is color coded to indicate whether it is in the past present future
 //click block can enter event
- // click button save button for the block the textfor the event is savedin local storage
- // i refresh page and events stay on page
- var today = moment();
- $("#currentDay").text(today.format("MMM Do, YYYY"));
+// click button save button for the block the textfor the event is savedin local storage
+// i refresh page and events stay on page
+
+const addTime = document.querySelector(".addstime");
+const repeatSection = document.querySelector(".input");
+
+let today = moment();
+$("#currentDay").text(today.format("MMM Do, YYYY"));
+
+const workTime = [
+  "8 a.m.",
+  "9 a.m.",
+  "10 a.m.",
+  "11 a.m.",
+  "12 p.m.",
+  "1 p.m.",
+  "2 p.m.",
+  "3 p.m.",
+  "4 p.m.",
+  "5 p.m.",
+];
+// uses arrow function to set items for the times
+const Item = (time) => `
+<div class="input d-flex ">
+<label class="addstime" for="label">${time}</label>
+<input class="flex-fill" type="textarea" id="label" maxlength="100%" />
+<button class='button' id='btn'>Save</button>
+</div>   
+`;
+//adds items through html starting with my array for work time and using map method to place the html code for every time avaibable
+$(".list-items").html(workTime.map(Item).join(""));
