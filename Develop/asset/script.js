b5b5e13451f8378cf75ca70c1e81cd6172bed73b
Jacobschoenbauer <jhwt65@gmail.com>
2022-09-30 11:54:52 -0500

    trying to save todos

--- Develop/asset/script.js
+++ Develop/asset/script.js
@@ -9,6 +9,10 @@
 
 const addTime = document.querySelector(".addstime");
 const repeatSection = document.querySelector(".input");
+const saveInfo = document.getElementById("label")
+const saveButton = document.getElementById("btn")
+const theUl = document.getElementById("listoftodos")
+var namie = []
 
 let today = moment();
 $("#currentDay").text(today.format("MMM Do, YYYY"));
@@ -25,24 +29,51 @@ const workTime = [
   "4 p.m.",
   "5 p.m.",
 ];
+
+function addAnswerToLocalStorage() {
+  const namie = saveInfo.value;
+  localStorage.setItem("answer", namie);
+}
+for (var i = 0; i < namie.length; i++) {
+  var nightName = namie[i];
+
+  
+
+namie[i] = saveInfo.textContent
+localStorage.setItem("namie", JSON.stringify(names));
+
+var storedNames = JSON.parse(localStorage.getItem("names")); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // uses arrow function to set items for the times
 const Item = (time) => `
 <div class="input d-flex ">
 <label class="addstime d-flex <div class="d-flex ">
 ${time}</label>
 <input class="flex-fill" type="textarea" id="label" maxlength="100%" />
+<ul id="listoftodos" class="d-flex">
 <button class='button' id='btn'>Save</button>
 </div>   
 `;
 //adds items through html starting with my array for work time and using map method to place the html code for every time avaibable
 $(".list-items").html(workTime.map(Item).join(""));
 
-let btnDsn = document.querySelector("#label");
-localStorage.setItem('click','label');
-let name = localStorage.getItem('Name');
-  
-(function (){
-    btnDsn.onclick = function() {
-        btnDsn.textContent =  label;
-    };
-})();
+
+
+
+
+saveButton.addEventListener("click", addAnswerToLocalStorage);
