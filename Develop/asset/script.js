9b06a6640603a903e5ed2aece180ce4a1fc9a82a
Jacobschoenbauer <jhwt65@gmail.com>
2022-10-01 14:46:52 -0500

    added javascript and css

--- Develop/asset/script.js
+++ Develop/asset/script.js
@@ -8,24 +8,49 @@
 // i refresh page and events stay on page
 
 
-
-
 let today = moment();
 $("#currentDay").text(today.format("MMM Do, YYYY"));
 
-
-
 $(document).ready(function () {
-  // set click function to save text to local storage 
+  // set click function to save text to local storage
   $(".button").on("click", function () {
-      // Get  values of the description in JQuery
-      var text = $(this).siblings(".thingstodo").val();
-      var time = $(this).parent().attr("id");
-      // Save text in local storage
-      
-      localStorage.setItem(time, text);
-  })
+    // Get  values of the description in JQuery
+    var text = $(this).siblings(".thingstodo").val();
+    var time = $(this).parent().attr("id");
+    // Save text in local storage
 
-  $("#6am.thingstodo").val(localStorage.getItem("6am"));
+    localStorage.setItem(time, text);
+    
+    
+   
+  
+  });
+  function timeTracker() {
+    //get current number of hours.
+    var timeNow = moment().hour();
 
-}) 
+    // loop over time blocks
+    $(".input").each(function () {
+        var blockTime = parseInt($(this).attr("id").split("addtime")[1]);
+
+        // To check the time and add the classes for background indicators
+        if (blockTime < timeNow) {
+            $(this).removeClass("future");
+            $(this).removeClass("present");
+            $(this).addClass("past");
+        }
+        else if (blockTime === timeNow) {
+            $(this).removeClass("past");
+            $(this).removeClass("future");
+            $(this).addClass("present");
+        }
+        else {
+            $(this).removeClass("present");
+            $(this).removeClass("past");
+            $(this).addClass("future");
+
+        }
+    })
+}
+timeTracker()
+});
